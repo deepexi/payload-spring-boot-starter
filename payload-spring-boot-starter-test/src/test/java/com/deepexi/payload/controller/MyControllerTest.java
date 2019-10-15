22c8b5cb870bd5f66c16775b999e28271e080cf6
xieyuanqing <yuanqingx@aliyun.com>
2019-10-15 19:40:05 +0800

    docs add .travis.yml

---
+++ payload-spring-boot-starter-test/src/test/java/com/deepexi/payload/controller/MyControllerTest.java
@@ -0,0 +1,47 @@
+package com.deepexi.payload.controller;
+
+import com.deepexi.payload.PayloadSpringBootStarterTestApplicationTests;
+import com.deepexi.payload.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class MyControllerTest extends PayloadSpringBootStarterTestApplicationTests {
+
+    @Test
+    public void get() throws Exception {
+        MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.get("/users/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        System.out.println("jsonObject = " + jsonObject);
+        Assert.assertEquals(jsonObject.getInt("code"), 1);
+        ObjectMapper objectMapper = new ObjectMapper();
+        User payload = objectMapper.readValue(jsonObject.getString("payload"), User.class);
+        Assert.assertNotNull(payload);
+
+    }
+
+    @Test
+    public void getErrorOfHtml() throws Exception {
+        MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.get("/users/0").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+    }
+
+    @Test
+    public void getErrorOfJson() throws Exception {
+        MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.get("/users/0").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println("result = " + result.getResponse().getContentAsString());
+    }
+}
